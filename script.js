9f1383fcbe18b3f5adefa37793480a794842b10a
isKazumi <Yudhistia110@gmail.com>
2022-11-12 03:01:28 +0800

    add slider

--- script.js
+++ script.js
@@ -34,3 +34,12 @@ function toggle(e) {
   if (this.closest('.has-child').classList != 'expand');
   this.closest('.has-child').classList.toggle('expand');
 }
+
+// slider
+const swiper = new Swiper('.swiper', {
+  loop: true,
+
+  pagination: {
+    el: '.swiper-pagination',
+  },
+});
